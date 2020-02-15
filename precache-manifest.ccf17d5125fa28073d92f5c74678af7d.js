9559d0231baae1e15dfbe029c3a09b432da47b5a
Fernando <f.sanchezvilela@gmail.com>
2020-02-15 16:00:19 +0100

    Updates

---
+++ precache-manifest.ccf17d5125fa28073d92f5c74678af7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458b1f01f06277bab287e032f498e895",
+    "url": "/dad-jokes-react/index.html"
+  },
+  {
+    "revision": "00948eccdab9499e89de",
+    "url": "/dad-jokes-react/static/css/main.34f85d8a.chunk.css"
+  },
+  {
+    "revision": "4bc728e8541e9c16eadc",
+    "url": "/dad-jokes-react/static/js/2.ea39d91b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dad-jokes-react/static/js/2.ea39d91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00948eccdab9499e89de",
+    "url": "/dad-jokes-react/static/js/main.73d86c75.chunk.js"
+  },
+  {
+    "revision": "b180341ab51825915318",
+    "url": "/dad-jokes-react/static/js/runtime-main.64f0b113.js"
+  }
+]);
